9a167db283f98b7db27e8c42b04897dd2927f2fc
JDDAR <jdanaconar@gmail.com>
2023-09-05 19:00:13 -0500

    Nuevos ejercicios del cino al ocho

---
+++ 007/app.js
@@ -0,0 +1,6 @@
+document.getElementById('icon-seach').onclick = function () { 
+  seachFuntion();
+};
+function seachFuntion() {
+  document.getElementById('input-seach').classList.toggle('seach-active');
+}
